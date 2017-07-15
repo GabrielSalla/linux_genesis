2e7cc7fc36bb70b7964e19f407fc5ddd3dc65087
Gabriel Salla <gabriel.salla@pagar.me>
2017-07-15 16:33:45 -0300

    Update script d0check.py

--- scripts/d0check.py
+++ scripts/d0check.py
@@ -1,14 +1,18 @@
 import opsutils
 from tabulate import tabulate
 
+
 def main():
     access_information = opsutils.load_access_information(look_at="home")
-    database_172 = opsutils.DatabaseHandler(access_information["database_172"])
+    database_handler = opsutils.DatabaseHandler(
+      access_information["database_replica"])
     slack = opsutils.Slack(access_information["slack_loro"]["token"])
 
-    building_bas = database_172.fetch("""
+    building_bas = database_handler.fetch("""
         select
           id,
+          company_id,
+          recipient_id,
           to_char(created_at at time zone 'America/Sao_Paulo', 'HH24:MI:SS') as time
         from "BulkAnticipations"
         where
@@ -18,9 +22,12 @@ def main():
     if(len(building_bas) == 0):
         slack.send_message("#risco", "D0 liberadas")
     else:
-        headers = ["id", "time"]
-        table = [(row["id"], row["time"]) for row in building_bas]
-        slack.send_message("@gabriel.salla", "```%s```" % tabulate(table, headers=headers))
+        headers = ["id", "company_id", "recipient_id", "time"]
+        table = [
+            (row["id"], row["company_id"], row["recipient_id"], row["time"])
+            for row in building_bas]
+        slack.send_message("@gabriel.salla", "```%s```" %
+                           tabulate(table, headers=headers))
 
 if(__name__ == "__main__"):
     main()
