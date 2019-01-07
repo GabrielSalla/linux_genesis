35f30877277ca0cc913170c745bc9e6c0789b511
Gabriel Salla <gabriel.c.salla@gmail.com>
2019-01-07 11:36:23 -0200

    Update pgcli pager settings

--- scripts/pgcli_config.sh
+++ scripts/pgcli_config.sh
@@ -1,2 +1,2 @@
-sed -i '/^pager = /c\pager = pspg -ibc0FX --line-numbers' ~/.config/pgcli/config
+sed -i '/^pager = /c\pager = pspg -ibc0FX --line-numbers --no-commandbar --no-mouse' ~/.config/pgcli/config
 sed -i '/\[named queries\]/r ./templates/pgcli_named_queries' ~/.config/pgcli/config
